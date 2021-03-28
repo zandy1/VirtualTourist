a26a522e4d373bfe71267e4663e3d90b098a11b4
Alexander Brown <zandy1@me.com>
2021-03-28 01:15:44 -0700

    Add files via upload
    feat: Added MapView
    
    Added a MapView to the storyboard, as this will be the initial view.  Created a new class for its ViewController - TravelMapViewController with a MKMapViewDelegate.

---
+++ VirtualTourist/VirtualTourist/TravelMapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TravelMapViewController.swift
+//  VirtualTourist
+//
+//  Created by Alexander Brown on 3/26/21.
+//
+
+import UIKit
+import MapKit
+
+class TravelMapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
