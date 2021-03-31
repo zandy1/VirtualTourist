1c09d8863573d71e58c4eae4e9584d594007a274
Alexander Brown <zandy1@me.com>
2021-03-30 23:14:29 -0700

    Add files via upload
    feat: Added Class PhotoAlbumViewController

---
+++ VirtualTourist/VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Alexander Brown on 3/30/21.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectionViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var collectionView: UICollectionView!
+    
+}
