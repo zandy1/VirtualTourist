3ac43e9e5e4a2e9ab94fae90bee3ff4f9264057b
Alexander Brown <zandy1@me.com>
2021-04-03 23:36:11 -0700

    Add files via upload
    feat:  Added UITapGestureRecognizer to TravelMapViewController

--- VirtualTourist/VirtualTourist/Controller/TravelMapViewController.swift
+++ VirtualTourist/VirtualTourist/Controller/TravelMapViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 import MapKit
 import CoreData
 
-class TravelMapViewController: UIViewController, MKMapViewDelegate {
+class TravelMapViewController: UIViewController, MKMapViewDelegate, UIGestureRecognizerDelegate {
     
     @IBOutlet var mapView: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(handleTap))
+            gestureRecognizer.delegate = self
+            mapView.addGestureRecognizer(gestureRecognizer)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func handleTap(gestureRecognizer: UILongPressGestureRecognizer) {
+        let location = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        // Add annotation:
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
     }
     
     /*
