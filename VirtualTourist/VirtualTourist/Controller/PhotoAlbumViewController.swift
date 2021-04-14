a8acdf2b752a864e923199f82e1e487d2718aa5e
Alexander Brown <zandy1@me.com>
2021-04-14 00:28:58 -0700

    Add files via upload
    feat: Updated method of downloading images

--- VirtualTourist/VirtualTourist/Controller/PhotoAlbumViewController.swift
+++ VirtualTourist/VirtualTourist/Controller/PhotoAlbumViewController.swift
@@ -30,9 +30,6 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
     var imagesPerPage: Int!
     var imagesAvailable: Int!
     
-    //var flickrImages: [UIImage]!
-
-    
     fileprivate func setupFetchedResultsController() {
         let fetchRequest:NSFetchRequest<Photo> = Photo.fetchRequest()
         let predicate = NSPredicate(format: "pin == %@", pin)
@@ -42,7 +39,6 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
         
         fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: nil)
         fetchedResultsController.delegate = self
-
         do {
             try fetchedResultsController.performFetch()
         } catch {
@@ -74,11 +70,13 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
           toDoWhenInView()
           if (pin.photo?.count == 0) {
               print("Pin Contains No Photos")
+              disableNewCollectionButton(isDisabled: true)
               getFlickrImages(page: 1)
           }
           else {
             restoreFlickrParams()
-            disableNewCollectionButton(isDisabled: false)
+            collectionView.reloadData()
+            disableNewCollectionButton(isDisabled: true)
           }
     }
     
@@ -105,56 +103,51 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
            self.navigationController?.navigationBar.isHidden = false
     }
     
-    func downloadImages() {
-        disableNewCollectionButton(isDisabled: true)
-    }
-    
-    
     func getFlickrImages(page: Int) {
         disableNewCollectionButton(isDisabled: true)
         FlickrAPI.searchPhotos(latitude: pin.latitude, longitude: pin.longitude, page: page, completion: self.handleGetFlickrImages(response:error:))
     }
-     
-    func handleGetFlickrImages(response: photosSearchResponse?, error: Error?) {
-       if (response != nil) {
-         currentPage = response!.photos.page
-         numPages = response!.photos.pages
-         imagesPerPage = response!.photos.perpage
-         imagesAvailable = Int(response!.photos.total)
-         saveFlickrParams()
-         //print(imagesAvailable)
-         if (imagesAvailable > 0) {
-            downloadImages()
-            for image in response!.photos.photo {
-                let photo = Photo(context: dataController.viewContext)
-                photo.image = nil
-                photo.url = image.computeURL()
-                photo.pin = self.pin
-                try? self.dataController.viewContext.save()
-                self.collectionView.reloadData()
-            }
-            //flickrImages = []
-            //for image in response!.photos.photo {
-                //addPhoto(urlString: image.computeURL())
-            //}
-            disableNewCollectionButton(isDisabled: false)
-         }
-         else {
-            noImagesFound()
-         }
-       }
-       else {
-          showFailure(failureType: "Unable To Get Images From Flickr", message: error?.localizedDescription ?? "")
-       }
-     }
     
+    func handleGetData(data: Data?, response: URLResponse?, error: Error?) -> Data?{
+        //let photo = Photo(context: dataController.viewContext)
+        if (error == nil) {
+           return(data)
+        }
+        else {
+          return(nil)
+        }
+    }
+    
+    func handleGetFlickrImages(response: photosSearchResponse?, error: Error?) {
+           if (response != nil) {
+             currentPage = response!.photos.page
+             numPages = response!.photos.pages
+             imagesPerPage = response!.photos.perpage
+             imagesAvailable = Int(response!.photos.total)
+             saveFlickrParams()
+             if (imagesAvailable > 0) {
+                for image in response!.photos.photo {
+                    let photo = Photo(context: dataController.viewContext)
+                    photo.url = image.computeURL()
+                    photo.image = nil
+                    photo.pin = self.pin
+                }
+                //collectionView.reloadData()
+             }
+             else {
+                noImagesFound()
+             }
+           }
+           else {
+              showFailure(failureType: "Unable To Get Images From Flickr", message: error?.localizedDescription ?? "")
+           }
+         }
     
     @IBAction func newCollectionTapped(_ sender: UIButton) {
         for photos in fetchedResultsController.fetchedObjects! {
             dataController.viewContext.delete(photos)
         }
         try? self.dataController.viewContext.save()
-        print(numPages)
         let randPage = Int.random(in: 1..<(numPages+1))
         getFlickrImages(page: randPage)
     }
@@ -162,18 +155,7 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
     func disableNewCollectionButton(isDisabled: Bool) {
         self.newCollectionButton.isHidden = isDisabled
     }
-     
-    func addPhoto(urlString: String) -> Data? {
-        print(urlString)
-        let url = URL(string: urlString)
-        guard let imageData = try? Data(contentsOf: url!) else {
-            return(nil)
-        }
-        DispatchQueue.main.async {
-        var downloadedImage: UIImage? { UIImage(data: imageData) }
-        }
-        return(imageData)
-    }
+    
        
     // Deletes the `Photo` at the specified index path
     func deletePhoto(at indexPath: IndexPath) {
@@ -203,6 +185,7 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
          self.present(alertVC, animated:true)
     }
     
+    // ToDo This doesn't work if there are multiple pins - need to add the params to the Photo entity instead of using UserDefaults
     func saveFlickrParams() {
         UserDefaults.standard.set(currentPage, forKey: "CurrentPage")
         UserDefaults.standard.set(numPages, forKey: "NumPages")
@@ -273,7 +256,7 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
     // MARK: UICollectionViewDataSource
 
     func collectionView(_ collectionView: UICollectionView, numberOfSections section: Int) -> Int {
-        fetchedResultsController.sections?.count ?? 1
+        fetchedResultsController.sections?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -281,8 +264,8 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
                 fatalError("No sections in fetchedResultsController")
             }
          let sectionInfo = sections[section]
-        return sectionInfo.numberOfObjects
-        //return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+        //return sectionInfo.numberOfObjects
+        return fetchedResultsController.sections?[0].numberOfObjects ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -290,16 +273,28 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoAlbumCollectionViewCell", for: indexPath) as! PhotoAlbumCollectionViewCell
         if (aPhoto.image == nil) {
             print("Need To Download Image")
+            self.disableNewCollectionButton(isDisabled: true)
             cell.spinner.startAnimating()
-            aPhoto.image = addPhoto(urlString: aPhoto.url!)
-            //try? self.dataController.viewContext.save()
-            cell.spinner.stopAnimating()
+            let url = URL(string: aPhoto.url!)
+            FlickrAPI.getData(from: url!) { data, response, error in
+              if error != nil { // Handle error…
+                  //completion(false,error)
+              }
+              else {
+                aPhoto.image = data
+                cell.photoAlbumImageView.image = UIImage(data: aPhoto.image!)
+                try? self.dataController.viewContext.save()
+                cell.spinner.stopAnimating()
+                self.disableNewCollectionButton(isDisabled: false)
+              }
+            }
         }
-        // image
-        cell.photoAlbumImageView.image = UIImage(data: aPhoto.image!)
+        else {
+          cell.photoAlbumImageView.image = UIImage(data: aPhoto.image!)
+        }
         return cell
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.myNavigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "trash"), style: .done, target: self, action: #selector(deleteCell))
         }
@@ -307,36 +302,39 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         self.myNavigationItem.rightBarButtonItem = UIBarButtonItem(title: "", style: .done, target: self, action: #selector(dummy))
         }
-
     
 }
 
 extension PhotoAlbumViewController:NSFetchedResultsControllerDelegate {
-   
-     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
-         switch type {
-         case .insert:
-             collectionView.insertItems(at: [newIndexPath!])
-             break
-         case .delete:
-             collectionView.deleteItems(at: [indexPath!])
-             break
-         case .update:
-            collectionView.reloadData()
-         default:
-             break
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+             switch type {
+             case .insert:
+                 collectionView.insertItems(at: [newIndexPath!])
+                 break
+             case .delete:
+                collectionView.deleteItems(at: [indexPath!])
+                 break
+             case .update:
+                collectionView.reloadData()
+                break
+             default:
+                 break
+             }
          }
-     }
+     
+         func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+             let indexSet = IndexSet(integer: sectionIndex)
+             switch type {
+             case .insert: collectionView.insertSections(indexSet)
+             break
+             case .delete: collectionView.deleteSections(indexSet)
+             break
+             case .update: collectionView.reloadData()
+             break
+             default:
+                  break
+             }
+         }
  
-     func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
-         let indexSet = IndexSet(integer: sectionIndex)
-         switch type {
-         case .insert: collectionView.insertSections(indexSet)
-         case .delete: collectionView.deleteSections(indexSet)
-         case .update: collectionView.reloadData()
-         default:
-              break
-         }
-     }
-  
 }
